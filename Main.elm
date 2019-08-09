7e160f49717379324277bd4e59cbd176325fb16a
True Ghiassi <true@ghiassitrio.co.uk>
2019-08-09 23:01:47 +0100

    Add button for inserting text.

--- Main.elm
+++ Main.elm
@@ -1,8 +1,11 @@
 module Main exposing (main)
 
 import Browser
+import Browser.Dom as Dom
 import Browser.Navigation as Nav
 import Element as E
+import Element.Input as Ei
+import Task
 import Url
 
 
@@ -26,26 +29,62 @@ view model =
 
 
 type alias Model =
-    { txt : String
+    { viewport : Dom.Viewport
+    , mode : Mode
     }
+
+
+type Mode
+    = InsertTextMode
+    | SelectorMode
 
 
 type Msg
     = DoNothing
+    | Viewport Dom.Viewport
+    | InsertText
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        InsertText ->
+            ( { model | mode = InsertTextMode }, Cmd.none )
+
+        Viewport vp ->
+            ( { model | viewport = vp }
+            , Cmd.none
+            )
+
         DoNothing ->
             ( model, Cmd.none )
 
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init _ url key =
-    ( { txt = "hello" }, Cmd.none )
+    ( { viewport = initViewport, mode = SelectorMode }
+    , Task.perform Viewport Dom.getViewport
+    )
+
+
+initViewport : Dom.Viewport
+initViewport =
+    { scene =
+        { width = 0
+        , height = 0
+        }
+    , viewport =
+        { x = 0
+        , y = 0
+        , width = 0
+        , height = 0
+        }
+    }
 
 
 homePage : Model -> E.Element Msg
 homePage model =
-    E.text "hello"
+    Ei.button []
+        { onPress = Just InsertText
+        , label = E.text "Insert text"
+        }
