6dd93f7cbd0a29d3bce342876779cc9f476a263e
True Ghiassi <true@ghiassitrio.co.uk>
2019-08-17 10:49:52 +0100

    Can now insert a text top node.

--- Main.elm
+++ Main.elm
@@ -3,7 +3,10 @@ module Main exposing (main)
 import Browser
 import Browser.Dom as Dom
 import Browser.Navigation as Nav
+import Debug
 import Element as E
+import Element.Border as Eb
+import Element.Events as Ev
 import Element.Input as Ei
 import Task
 import Url
@@ -31,23 +34,146 @@ view model =
 type alias Model =
     { viewport : Dom.Viewport
     , mode : Mode
+    , website : Maybe Website
+    , focussedNode : Maybe Int
+    , internalErr : Maybe String
+    , maxId : Int
     }
+
+
+type alias Website =
+    ( Component, Int )
+
+
+type Component
+    = Text String
+    | Row (List Website)
+    | Column (List Website)
 
 
 type Mode
     = InsertTextMode
     | SelectorMode
+    | EditComponentMode
 
 
 type Msg
     = DoNothing
     | Viewport Dom.Viewport
     | InsertText
+    | EmptyClick
+    | EditComponent
+    | EditText Int String
+    | Focussed Int
+    | Unfocussed Int
+    | Unselect
+
+
+notEmptyErr =
+    "received EmptyClick but the website is not empty"
+
+
+nothingToSelectErr =
+    "selection mode is not allowed when there is nothing to select"
+
+
+nothingToEditErr =
+    "edit mode is not allowed when there is nothing to edit"
+
+
+createTopNode : Model -> ( Model, Cmd Msg )
+createTopNode model =
+    case model.mode of
+        InsertTextMode ->
+            ( { model
+                | website = Just ( Text "Some new text", 0 )
+                , focussedNode = Just 0
+                , maxId = 0
+              }
+            , Cmd.none
+            )
+
+        EditComponentMode ->
+            ( { model | internalErr = Just nothingToEditErr }
+            , Cmd.none
+            )
+
+        SelectorMode ->
+            ( { model | internalErr = Just nothingToSelectErr }
+            , Cmd.none
+            )
+
+
+updateTextNode : Int -> String -> Website -> Website
+updateTextNode id newText ( component, oldId ) =
+    case ( component, oldId == id ) of
+        ( Text oldText, True ) ->
+            ( Text newText, id )
+
+        _ ->
+            case component of
+                Row websites ->
+                    ( Row <|
+                        List.map
+                            (updateTextNode id newText)
+                            websites
+                    , oldId
+                    )
+
+                Column websites ->
+                    ( Column <|
+                        List.map (updateTextNode id newText) websites
+                    , oldId
+                    )
+
+                _ ->
+                    ( component, oldId )
+
+
+editEmptyErr =
+    "can't edit text in empty website"
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        Unselect ->
+            ( { model | focussedNode = Nothing }, Cmd.none )
+
+        Focussed id ->
+            ( { model | focussedNode = Just id }, Cmd.none )
+
+        Unfocussed id ->
+            ( { model | focussedNode = Nothing }, Cmd.none )
+
+        EditText id newText ->
+            case model.website of
+                Just ws ->
+                    ( { model
+                        | website =
+                            Just <| updateTextNode id newText ws
+                      }
+                    , Cmd.none
+                    )
+
+                Nothing ->
+                    ( { model | internalErr = Just editEmptyErr }
+                    , Cmd.none
+                    )
+
+        EditComponent ->
+            ( { model | mode = EditComponentMode }, Cmd.none )
+
+        EmptyClick ->
+            case model.website of
+                Just _ ->
+                    ( { model | internalErr = Just notEmptyErr }
+                    , Cmd.none
+                    )
+
+                Nothing ->
+                    createTopNode model
+
         InsertText ->
             ( { model | mode = InsertTextMode }, Cmd.none )
 
@@ -62,7 +188,13 @@ update msg model =
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init _ url key =
-    ( { viewport = initViewport, mode = SelectorMode }
+    ( { viewport = initViewport
+      , mode = SelectorMode
+      , website = Nothing
+      , internalErr = Nothing
+      , focussedNode = Nothing
+      , maxId = 0
+      }
     , Task.perform Viewport Dom.getViewport
     )
 
@@ -84,7 +216,107 @@ initViewport =
 
 homePage : Model -> E.Element Msg
 homePage model =
-    Ei.button []
-        { onPress = Just InsertText
-        , label = E.text "Insert text"
-        }
+    E.row [ E.width E.fill ] [ emptyOrErrOrFull model model.website model.internalErr, tools model ]
+
+
+emptyOrErrOrFull : Model -> Maybe Website -> Maybe String -> E.Element Msg
+emptyOrErrOrFull model website err =
+    case err of
+        Just e ->
+            E.text e
+
+        Nothing ->
+            case website of
+                Nothing ->
+                    emptySite
+
+                Just something ->
+                    showWebsite model something
+
+
+emptySite : E.Element Msg
+emptySite =
+    E.row
+        [ E.width E.fill
+        , E.height E.fill
+        , Ev.onClick EmptyClick
+        ]
+        []
+
+
+focusses id =
+    [ Ev.onClick (Focussed id)
+    ]
+
+
+showText : String -> Int -> E.Element Msg
+showText txt id =
+    E.el (focusses id) <|
+        E.text txt
+
+
+showWebsite : Model -> Website -> E.Element Msg
+showWebsite model website =
+    E.el [ E.width E.fill ] <|
+        case website of
+            ( Text text, id ) ->
+                case ( Debug.log "focussedId" model.focussedNode, model.mode ) of
+                    ( Just focussedId, EditComponentMode ) ->
+                        if focussedId == id then
+                            Ei.multiline (focusses id)
+                                { onChange = EditText id
+                                , text = text
+                                , placeholder =
+                                    Just <|
+                                        Ei.placeholder [] <|
+                                            E.text "type text here"
+                                , label = Ei.labelHidden "text entry box"
+                                , spellcheck = True
+                                }
+
+                        else
+                            showText text id
+
+                    _ ->
+                        showText text id
+
+            -- case model.focussedNode of
+            --     Just focussedId ->
+            --         if focussedId == id then
+            --             Ei.multiline []
+            --                 { onChange = EditText id
+            --                 , text = text
+            --                 , placeholder =
+            --                     Just <|
+            --                         Ei.placeholder [] <|
+            --                             E.text "type text here"
+            --                 , label = Ei.labelHidden "text entry box"
+            --                 , spellcheck = True
+            --                 }
+            --         else
+            --             E.text text
+            --     Nothing ->
+            --         E.text text
+            ( Row websites, id ) ->
+                E.row [] <| List.map (showWebsite model) websites
+
+            ( Column websites, id ) ->
+                E.column [] <| List.map (showWebsite model) websites
+
+
+tools : Model -> E.Element Msg
+tools model =
+    E.column []
+        [ Ei.button []
+            { onPress = Just InsertText
+            , label = E.text "Insert text"
+            }
+        , Ei.button []
+            { onPress = Just EditComponent
+            , label = E.text "Edit"
+            }
+        , Ei.button []
+            { onPress = Just Unselect
+            , label = E.text "Unselect"
+            }
+        ]
